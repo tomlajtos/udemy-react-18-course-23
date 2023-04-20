c63e0c0eb16f1ddc819e3699a638242e30cb30e4
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-04-20 13:48:44 +0200

    Refactor nav-links and nav-icons

--- 02-backroads-project/learn/backroads-app/src/data.js
+++ 02-backroads-project/learn/backroads-app/src/data.js
@@ -3,10 +3,10 @@ export const pageLinks = [
   { id: 2, href: '#about', text: 'about' },
   { id: 3, href: '#services', text: 'services' },
   { id: 4, href: '#tours', text: 'tours' },
-]
+];
 
 export const socialLinks = [
   { id: 1, href: 'https://www.twitter.com', socialIcon: 'fab fa-facebook' },
   { id: 2, href: 'https://www.twitter.com', socialIcon: 'fab fa-twitter' },
   { id: 3, href: 'https://www.twitter.com', socialIcon: 'fab fa-squarespace' },
-]
+];
