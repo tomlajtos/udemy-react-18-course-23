c7bfc5e2a31d6173982053ba269327a0fa04ad1a
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-04-23 21:13:48 +0200

    Add Service and refactor Services

--- 02-backroads-project/learn/backroads-app/src/components/Services.js
+++ 02-backroads-project/learn/backroads-app/src/components/Services.js
@@ -1,20 +1,12 @@
 import Title from "./Title";
-import {services} from "../data.js"
+import Service from "./Service";
+import { services } from "../data.js"
 
 const Services = () => {
   return (
     <section className="section services" id="services">
-    <Title text1="our" text2="services"/>
-      <div className="section-center services-center">
-  {services.map(service => 
-        <article key={service.id} className="service">
-          <span className="service-icon"><i className={service.icon}></i></span>
-          <div className="service-info">
-            <h4 className="service-title">{service.title}</h4>
-            <p className="service-text">{service.text}</p>
-          </div>
-        </article>)}
-      </div>
+      <Title text1="our" text2="services" />
+      <Service services={services} />
     </section>
   );
 };
