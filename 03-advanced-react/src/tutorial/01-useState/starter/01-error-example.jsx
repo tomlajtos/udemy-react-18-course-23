e1453433082294d5011af30d25307c97eaffb821
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-02 13:42:10 +0200

    Add button click counter w/o useState

--- 03-advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,33 @@
+// set up counter without state hook and see nothing happenig
+// used inline styles because of me being lazy
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let count = 0;
+  const clickFn = () => {
+    count += 1;
+    console.log(count);
+  };
+  return (
+    <>
+      <h2>useState error example</h2>
+      <h3 style={{ color: "navy" }}>Num of clicks: {count}</h3>
+      <button
+        style={{
+          color: "red",
+          height: "2rem",
+          fontSize: "1rem",
+          margin: "0 auto ",
+          padding: "0 5px",
+          width: "fit-content",
+          display: "block",
+          border: "black solid 2px",
+          borderRadius: "10px",
+        }}
+        onClick={clickFn}
+      >
+        Click me daddy!
+      </button>
+    </>
+  );
 };
 
 export default ErrorExample;
