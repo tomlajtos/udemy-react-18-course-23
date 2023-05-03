e8e97ed7f51405f85d87d32a941b3d6cf7724636
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-03 23:08:18 +0200

    feat: Add user list with remove-buttons

--- 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import { data } from "../../../data.js";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [userData, setUserData] = useState(data);
+  const clearData = () => setUserData([]);
+
+  return (
+    <>
+      <h2>useState array challenge</h2>
+      <ul>
+        {userData.map((user) => {
+          console.log(user.name);
+          return (
+            <li key={user.id}>
+              <article>
+                <h3>{user.name}</h3>
+                <button type="button">click</button>
+              </article>
+            </li>
+          );
+        })}
+      </ul>
+      <button type="button" onClick={clearData}>
+        big click
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
