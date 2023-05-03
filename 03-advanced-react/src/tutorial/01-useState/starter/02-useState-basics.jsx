4295d483186f0b4df5e1137b464a065f6381d8d0
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-03 09:59:34 +0200

    refactor: Change button style to default, add b. type

--- 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -9,20 +9,7 @@ const UseStateBasics = () => {
     <>
       <h2>useState basics</h2>;
       <h3 style={{ color: "navy" }}>Num of clicks: {count}</h3>
-      <button
-        style={{
-          color: "red",
-          height: "2rem",
-          fontSize: "1rem",
-          margin: "0 auto ",
-          padding: "0 5px",
-          width: "fit-content",
-          display: "block",
-          border: "black solid 2px",
-          borderRadius: "10px",
-        }}
-        onClick={clickFn}
-      >
+      <button className="btn" type="button" onClick={clickFn}>
         Click Cick Click!
       </button>
     </>
