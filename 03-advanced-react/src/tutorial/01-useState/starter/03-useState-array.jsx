96d57f8bc0dabe8cc3949261616d8f9ffa010e47
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-04 17:33:28 +0200

    Add styles
    Add styles
    
    Add styles

--- 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -5,15 +5,22 @@ const removedData = [];
 
 const UseStateArray = () => {
   const [users, setUsers] = useState(data);
-  const [removedUsers, setRemovedUsers] = useState(removedData);
 
   // handler functions, onClick
   const clearUsers = () => {
     removedData.push(...data.splice(0, data.length));
     setUsers([...data]);
   };
+  const restoreUsers = () => {
+    data.push(...removedData.splice(0, removedData.length));
+    setUsers([...data]);
+  };
   const removeUser = (i) => {
     removedData.push(...data.splice(i, 1));
+    setUsers([...data]);
+  };
+  const restoreUser = (i) => {
+    data.push(...removedData.splice(i, 1));
     setUsers([...data]);
   };
   return (
@@ -23,22 +30,46 @@ const UseStateArray = () => {
       <ul className="users">
         {users.map(({ id, name }, index) => {
           return (
-            <li key={id} className="user">
+            <li key={id}>
               <h3>{name}</h3>
+              <p style={{ color: "lightseagreen" }}>User ID: {id}</p>
               <button
                 type="button"
-                className="del-btn"
+                className="btn"
                 onClick={() => removeUser(index)}
               >
-                click
+                remove
               </button>
             </li>
           );
         })}
       </ul>
       {/*delete all*/}
-      <button type="button" className="btn clear-btn" onClick={clearUsers}>
-        big click
+      <button type="button" className="clr-all-btn" onClick={clearUsers}>
+        remove all
+      </button>
+
+      <hr className="hr" />
+
+      <ul className="users removedUsers">
+        {removedData.map(({ id, name }, index) => {
+          return (
+            <li key={id}>
+              <h3>{name}</h3>
+              <p style={{ color: "lightcoral" }}>User ID: {id}</p>
+              <button
+                type="button"
+                className="btn"
+                onClick={() => restoreUser(index)}
+              >
+                restore
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      <button type="button" className="res-all-btn" onClick={restoreUsers}>
+        restore all
       </button>
     </>
   );
