15573ef74780c3bca4a13c4781133d97f21f5377
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-04 14:57:48 +0200

    feat: Add function to remove individual users

--- 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -11,18 +11,23 @@ const UseStateArray = () => {
     removedData.push(...data.splice(0, data.length));
     setUsers([...data]);
   };
+  const removeUser = (i) => {
+    removedData.push(...data.splice(i, 1));
+    setUsers([...data]);
+  };
   return (
     <>
       <h2>useState array challenge</h2>
 
       <ul className="users">
-        {users.map(({ id, name }) => {
+        {users.map(({ id, name }, index) => {
           return (
             <li key={id} className="user">
               <h3>{name}</h3>
               <button
                 type="button"
                 className="del-btn"
+                onClick={() => removeUser(index)}
               >
                 click
               </button>
