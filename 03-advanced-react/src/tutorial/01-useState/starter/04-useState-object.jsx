23f8866032201b02aee214c36c0decebb8b54c97
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-05 21:46:04 +0200

    Add solution for useState object

--- 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,29 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "mick",
+    age: 69,
+    hobby: "singing",
+  });
+
+  const displayPerson = () => {
+    // setPerson({ name: "Joel", age: 33, hobby: "writing" });
+    // change only one values
+    // setPerson({ ...person, name: "Jean-Paul" });
+  };
+
+  return (
+    <>
+      <h2>useState object example</h2>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>Enjoys: {person.hobby}</h4>
+      <button className="btn" type="button" onClick={displayPerson}>
+        show john
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
