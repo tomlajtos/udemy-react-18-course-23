031b2b29c8840798d1814dd3bbdd7c726655c4bc
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-05 22:12:32 +0200

    Add solution and comment for useState gotcha

--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [number, setNumber] = useState(0);
+  const clickFn = () => {
+    setNumber(number + 1);
+    console.log(number); // useState is not sinc.,
+    // this will not log the latest value, but the one before
+  };
+  return (
+    <>
+      <h2>useState "gotcha"</h2> <p>This goes up: {number}</p>
+      <button className="btn" type="button" onClick={clickFn}>
+        +1
+      </button>
+    </>
+  );
 };
 
 export default UseStateGotcha;
