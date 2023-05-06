4798e167c4887b6c24b949c13050a7201619a7de
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-06 22:11:31 +0200

    Add useEffect hook and comments about basics

--- 03-advanced-react/src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
   const sayHello = () => {
-    console.log('hello there');
+    console.log("hello there", value);
   };
 
   sayHello();
-
+  useEffect(() => {
+    // 2 args: 1st callback, 2nd opt. dependency-array, 1st arg cannot be set up as async
+    // however, **inside** the callback func we **can** set up an async function.
+    console.log("Yay, useEffect hook in action...!");
+  }, []); // if empty array, function only envoked at first render
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>
