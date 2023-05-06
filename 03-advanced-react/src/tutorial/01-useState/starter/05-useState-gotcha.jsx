70a861891427b9bc0af5e3e807792428201ffd9e
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-06 21:06:58 +0200

    Solution for setState not showing the current value

--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -3,13 +3,24 @@ import { useState } from "react";
 const UseStateGotcha = () => {
   const [number, setNumber] = useState(0);
   const clickFn = () => {
-    setNumber(number + 1);
-    console.log(number); // useState is not sinc.,
-    // this will not log the latest value, but the one before
+    /**
+     * gotcha example:
+     * setNumber(number + 1);
+     * console.log(number); // useState is not sinc.,
+     * // this will not log the latest value, but the one before
+     */
+    // solution for above problem:
+    setNumber((currentState) => {
+      //passing a function as an arg into 'setState'
+      const newState = currentState + 1;
+      console.log(newState);
+      return newState; //it must return a value
+    });
   };
   return (
     <>
-      <h2>useState "gotcha"</h2> <p>This goes up: {number}</p>
+      <h2>useState "gotcha"</h2>
+      <h3>This goes up: {number}</h3>
       <button className="btn" type="button" onClick={clickFn}>
         +1
       </button>
