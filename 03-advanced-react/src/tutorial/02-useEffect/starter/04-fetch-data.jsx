aef0ac4cef940fdacecf45d56fcb3957e2350341
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-08 14:23:24 +0200

    Add FetchData component with useEffect and useState

--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,32 @@
-const url = 'https://api.github.com/users';
+import { useState, useEffect } from "react";
+
+const url = "https://api.github.com/users";
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await fetch(url);
+      // const data = await response.json();
+      setUsers(await response.json());
+    };
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      <h2>fetch data example</h2>
+      <ul>
+        <h2>users list</h2>
+        {users.map((user) => (
+          <li key={user.id}>
+            <img src={user.avatar_url} alt={user.login} />
+            <h3>{user.login}</h3>
+            <a href={user.html_url}>link</a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 export default FetchData;
