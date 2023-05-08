b6f5d57b87e06dee465f631778b3835db4beaa12
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-08 22:51:01 +0200

    Add example comp for multiple returns basics

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
@@ -1,6 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const MultipleReturnsBasics = () => {
-  return <h2>Multiple Returns Basics</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("done fetching the data");
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>Multiple Returns Basics</h2>
+      <div>yo</div>
+    </>
+  );
 };
 export default MultipleReturnsBasics;
