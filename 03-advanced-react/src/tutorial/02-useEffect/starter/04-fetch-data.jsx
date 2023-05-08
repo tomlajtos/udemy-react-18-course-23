0402ef95ebfc1162d62a718e0347d7b277b1bfb6
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-08 17:44:24 +0200

    Fix: typo in api url

--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,7 +1,7 @@
 import "./04-fetch-data.css";
 import { useState, useEffect } from "react";
 
-const url = "https://avi.github.com/users";
+const url = "https://api.github.com/users";
 
 const FetchData = () => {
   const [users, setUsers] = useState([]);
