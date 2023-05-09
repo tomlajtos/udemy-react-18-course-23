9da7badcf3d08bde62f52d6f3a1d5115dffbec0f
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-09 12:29:50 +0200

    feature: Display loading and error

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -12,12 +14,25 @@ const MultipleReturnsFetchData = () => {
         const data = await response.json();
         setUser(data);
       } catch (error) {
+        //fetch will not throw when it is 404
+        setIsError(true);
         console.log(error);
       }
+      setIsLoading(false);
     })();
   }, []);
+  /** REMINDER : Order Matters !!!
+   * code is executed top to bottom so condition for isLoading
+   * should come before the jsx.
+   */
 
-  console.log(user);
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2>There was an error...</h2>;
+  }
+
   return (
     <>
       <h2>Fetch Data </h2>
