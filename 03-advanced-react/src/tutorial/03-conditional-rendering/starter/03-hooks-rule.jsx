39f187de48328a5819880937b9ef38254dc3a035
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-09 22:29:47 +0200

    Add some comments

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/03-hooks-rule.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/03-hooks-rule.jsx
@@ -1,19 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const Example = () => {
   const [condition, setCondition] = useState(true);
   if (condition) {
-    // won't work
+    // won't work, do not declare useState consditionally
     const [state, setState] = useState(false);
   }
 
-  // if (condition) {
-  //   return <h2>Hello There</h2>;
-  // }
-  // // this will also fail
-  // useEffect(() => {
-  //   console.log('hello there');
-  // }, []);
+  if (condition) {
+    return <h2>Hello There</h2>;
+  }
+  // this will also fail, useEffect should be before the conditions
+  useEffect(() => {
+    console.log("hello there");
+  }, []);
   return <h2>example</h2>;
 };
 
