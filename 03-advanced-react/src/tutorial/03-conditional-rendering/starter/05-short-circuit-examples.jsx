09f533016ae0bf6e35f5f39f0ee5baf4b536c670
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-10 14:08:55 +0200

    Add ternary operator examples

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -5,19 +5,36 @@ const ShortCircuitExamples = () => {
   const [text, setText] = useState("");
   // truthy
   const [name, setName] = useState("susan");
-  const [user, setUser] = useState({ name: "john" });
-  const [isEditing, setIsEditing] = useState(false);
+  const [user, setUser] = useState({ name: "Dave" });
+  const [isEditing, setIsEditing] = useState(true);
 
   return (
     <>
+      <hr style={{ margin: "20px auto" }} />
       <h2>{text || "short circuit - examples"}</h2>
+      <hr style={{ margin: "20px auto" }} />
       {text && (
         <div>
           <h2>hmmmmm</h2>
           <h2>{name}</h2>
         </div>
       )}
-      {user && <UserComponent name={user.name} j />}
+      {user && <UserComponent name={user.name} />}
+      <hr style={{ margin: "20px auto" }} />
+      <h2>ternary operator example</h2>
+      <button className={"btn"} type="button">
+        {isEditing ? "edit" : "add"}
+      </button>
+      <hr style={{ visibility: "hidden", margin: "20px auto" }} />
+      {user ? (
+        <div>
+          <h4>Hello there {user.name}</h4>
+        </div>
+      ) : (
+        <div>
+          <h2>Please login</h2>
+        </div>
+      )}
     </>
   );
 };
