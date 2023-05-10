df806c170540a661ab3643600a1624dbdb61fccf
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-10 22:15:40 +0200

    Add component toggled by button click (challenge)

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,48 @@
+import { useState, useEffect } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [isRendered, setIsRendered] = useState(false);
+  const handleClick = () => {
+    !isRendered ? setIsRendered(true) : setIsRendered(false);
+  };
+
+  return (
+    <>
+      <h2>toggle challenge</h2>
+      <button className={"btn"} type="button" onClick={handleClick}>
+        toggle component
+      </button>
+      {isRendered && <TestComponent />}
+    </>
+  );
+};
+
+const TestComponent = () => {
+  return (
+    <div
+      style={{
+        margin: "30px auto",
+        padding: "5px",
+        width: "fit-content",
+        border: "8px solid #6131ff",
+        borderRadius: "25px",
+      }}
+    >
+      <h2
+        style={{
+          margin: "auto",
+          padding: "20px",
+          width: "fit-content",
+          color: "#efefef",
+          backgroundColor: "#6131ff",
+          border: "1px solid #cfcfcf",
+          borderRadius: "15px",
+        }}
+      >
+        This component renders on button click
+      </h2>
+    </div>
+  );
 };
 
 export default ToggleChallenge;
