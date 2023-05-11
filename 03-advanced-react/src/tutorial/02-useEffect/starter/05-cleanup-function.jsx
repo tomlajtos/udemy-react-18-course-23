64b200f7bd093a24b4461cb1f53203edd5f41ec5
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-11 14:01:36 +0200

    Render component with useEffect conditionally

--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,40 @@
+import { useState, useEffect } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [isLoading, setIsLoading] = useState(false);
+  const load = () => {
+    setIsLoading(!isLoading);
+  };
+
+  return (
+    <>
+      <h2>cleanup function</h2>
+      <hr style={{ margin: "20px auto" }} />
+      {isLoading ? <SecondComp /> : <FirstComp />}
+      <button className={"btn"} type="button" onClick={load}>
+        {isLoading ? "go back" : "load next"}
+      </button>
+    </>
+  );
+};
+
+const FirstComp = () => {
+  return <h2>This is component 1</h2>;
+};
+
+const SecondComp = () => {
+  const [text, setText] = useState("2nd component is loading...");
+
+  // use effect will run every time the component renders, not just at initial render
+  // the component mounts/unmounts on button click, basically
+  useEffect(() => {
+    console.log("useEffect...");
+  }, []);
+  return (
+    <>
+      <h2>{text}</h2>
+    </>
+  );
 };
 
 export default CleanupFunction;
