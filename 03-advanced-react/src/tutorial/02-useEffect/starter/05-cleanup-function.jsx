dd3978f33598ae0ae2f5148d8e5cd04f099c0627
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-11 23:30:30 +0200

    wip: Add very hacky comp. loading/reloading

--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,19 +1,47 @@
+//TODO: figure out re-render function for 2nd component
 import { useState, useEffect } from "react";
 
 const CleanupFunction = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  const [reload, setReload] = useState(false);
+  const [text, setText] = useState("2nd component is loading");
+  const [delay, setDelay] = useState(9000);
+
   const load = () => {
-    setIsLoading(!isLoading);
+    setToggle(!toggle);
   };
 
   return (
     <>
       <h2>cleanup function</h2>
       <hr style={{ margin: "20px auto" }} />
-      {isLoading ? <SecondComp /> : <FirstComp />}
+      {toggle ? (
+        <SecondComp setReload={setReload} delay={delay} text={text} />
+      ) : (
+        <FirstComp />
+      )}
       <button className={"btn"} type="button" onClick={load}>
-        {isLoading ? "go back" : "load next"}
+        {toggle ? "go back" : "load next"}
       </button>
+      {reload && (
+        <button
+          className="btn"
+          style={{
+            backgroundColor: "orange",
+            fontWeight: "800",
+            marginLeft: "10px",
+          }}
+          type="button"
+          onClick={() => {
+            setDelay(1000); //this is not working properly... hence TODO
+            setText("reloading component");
+            setReload(false);
+          }}
+        >
+          {" "}
+          reload
+        </button>
+      )}
     </>
   );
 };
@@ -22,18 +50,96 @@ const FirstComp = () => {
   return <h2>This is component 1</h2>;
 };
 
-const SecondComp = () => {
-  const [text, setText] = useState("2nd component is loading...");
+const SecondComp = ({ setReload, delay, text }) => {
+  const [count, setCount] = useState(0);
+  const [isData, setIsData] = useState(false);
+  let [progress, setProgress] = useState(". ");
 
   // use effect will run every time the component renders, not just at initial render
   // the component mounts/unmounts on button click, basically
   useEffect(() => {
     console.log("useEffect...");
-  }, []);
+    setTimeout(() => {
+      setProgress((progress += ". "));
+      !isData ? setCount(count + 1) : isData;
+    }, 800);
+
+    setTimeout(() => {
+      setIsData(true);
+    }, delay);
+
+    if (count >= 10) {
+      setReload(true);
+    }
+  }, [count, delay]);
+
   return (
     <>
-      <h2>{text}</h2>
+      {!isData && count < 10 ? (
+        <div>
+          <h2>{text}</h2>
+          <h2>{progress}</h2>
+        </div>
+      ) : !isData || count >= 10 ? (
+        <h2>something ain't right here...refresh the browser maybe?</h2>
+      ) : (
+        <Data />
+      )}
     </>
+  );
+};
+
+// test data for fake fatch
+const Data = () => {
+  return (
+    <div>
+      <h2>This is what you've been waiting for</h2>
+      <div
+        style={{
+          margin: "20px auto",
+          width: "fit-content",
+          height: "fit-content",
+          border: "20px solid red",
+          borderRadius: "50%",
+        }}
+      >
+        <div
+          style={{
+            width: "fit-content",
+            height: "fit-content",
+            border: "20px solid white",
+            borderRadius: "50%",
+          }}
+        >
+          <div
+            style={{
+              width: "fit-content",
+              height: "fit-content",
+              border: "20px solid red",
+              borderRadius: "50%",
+            }}
+          >
+            <div
+              style={{
+                width: "fit-content",
+                height: "fit-content",
+                border: "20px solid white",
+                borderRadius: "50%",
+              }}
+            >
+              <div
+                style={{
+                  width: "100px",
+                  height: "100px",
+                  borderRadius: "50%",
+                  backgroundColor: "red",
+                }}
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
