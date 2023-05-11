e171178d32db5af0cbd1308c12b0cc0ff8798780
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-11 10:41:07 +0200

    Add conditional rendering for login/out

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+
+  const login = () => {
+    setUser({ name: "Max" });
+  };
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <>
+      <h2>user challenge</h2>
+      {user ? (
+        <div>
+          <h4>hello there, {user.name}</h4>
+          <button className="btn" type="button" onClick={logout}>
+            logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h4>please login</h4>
+          <button className="btn" type="button" onClick={login}>
+            login
+          </button>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default UserChallenge;
